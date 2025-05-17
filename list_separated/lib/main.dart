c896e1aa2691757cf09809d7b897230d2169dddb
Maheen Qamar <mq254411@gmail.com>
2025-05-17 10:50:45 -0700

    separatorBuilder

---
+++ list_separated/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  var arrFruits = ['Apple', 'Mango', 'Orange', 'Banana'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.only(top: 30, bottom: 30 , left: 10 , right: 10 ),
+        itemBuilder: (context, index) {
+          return Text(
+            arrFruits[index],
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          );
+        },
+        itemCount: arrFruits.length,
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: 100,
+            thickness: 5,
+            color: Colors.cyan,
+          );
+        },
+      ),
+    ); // ← this was missing or incorrectly closed
+  }
+}
